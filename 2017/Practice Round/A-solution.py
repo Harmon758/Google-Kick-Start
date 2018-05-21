133f449f92468cb5841737a370e684d748c250a3
Harmon <Harmon758@gmail.com>
2018-05-21 15:24:19 -0500

    Add 2017 Practice Round Problem A

---
+++ 2017/Practice Round/A-solution.py
@@ -0,0 +1,16 @@
+
+# Problem A. Country Leader
+# https://code.google.com/codejam/contest/6304486/dashboard#s=p0
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    leader = input()
+    greatest = len(set(leader.replace(" ", "")))
+    for i in range(N - 1):
+        name = input()
+        count = len(set(name.replace(" ", "")))
+        if count > greatest or (count == greatest and name < leader):
+            greatest = count
+            leader = name
+    print(f"Case #{t + 1}: {leader}")
