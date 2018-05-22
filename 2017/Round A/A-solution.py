8a8beb1aa03b59d203f7914a9d79827f001fbd6b
Harmon <Harmon758@gmail.com>
2018-05-21 19:55:19 -0500

    Add 2017 Round A Problem A

---
+++ 2017/Round A/A-solution.py
@@ -0,0 +1,10 @@
+
+# Problem A. Square Counting
+# https://code.google.com/codejam/contest/8284486/dashboard#s=p0
+
+T = int(input())
+for t in range(T):
+    R, C = map(int, input().split())
+    S1, S2 = min(R, C), max(R, C)
+    squares = (S1 * (S1 - 1) * (S1 + 1) * (2 * S2 - S1) // 12) % (10 ** 9 + 7)
+    print(f"Case #{t + 1}: {squares}")
