5e55fc067296a88c15550903ba11567308a0d8c8
Harmon <Harmon758@gmail.com>
2018-07-28 05:57:08 -0500

    Add 2017 Round B Problem A

---
+++ 2017/Round B/A-solution.py
@@ -0,0 +1,18 @@
+
+# Problem A. Math Encoder
+# https://code.google.com/codejam/contest/11304486/dashboard#s=p0
+
+powers_of_2 = [1]
+for i in range(1, 10000):
+    powers_of_2.append(2 * powers_of_2[-1])
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    K = list(map(int, input().split()))
+    total = 0
+    for n in range(N):
+        end = N - n - 1
+        for i in range(end):
+            total += (K[end] - K[i]) * powers_of_2[max(end - i - 1, 0)]
+    print(f"Case #{t + 1}: {total % (10 ** 9 + 7)}")
