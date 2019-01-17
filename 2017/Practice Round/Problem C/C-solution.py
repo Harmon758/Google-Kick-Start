a08465065a0d7512ab0b33e6ed6f7839dae67953
Harmon <Harmon758@gmail.com>
2019-01-17 07:12:22 -0600

    Remove header comments from 2017 Practice Round Problem C solution

--- 2017/Practice Round/Problem C/C-solution.py
+++ 2017/Practice Round/Problem C/C-solution.py
@@ -1,7 +1,3 @@
-
-# Problem C. Sherlock and Parentheses
-# https://code.google.com/codejam/contest/6304486/dashboard#s=p2
-
 T = int(input())
 for t in range(T):
     L, R = map(int, input().split())
