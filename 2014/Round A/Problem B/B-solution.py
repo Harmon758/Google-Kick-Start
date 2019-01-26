e8e358f8f6269e666ea1470baf52e44394de83eb
Harmon <Harmon758@gmail.com>
2019-01-25 20:15:45 -0600

    Add 2014 Round A Problem B

---
+++ 2014/Round A/Problem B/B-solution.py
@@ -0,0 +1,35 @@
+T = int(input())
+for t in range(T):
+    N, DIR = input().split()
+    N = int(N)
+    board = []
+    for row in range(N):
+        board.append(list(map(int, input().split())))
+    if DIR in ("up", "down"):
+        board = [list(column) for column in zip(*board)]
+    if DIR in ("right", "down"):
+        board = [row[::-1] for row in board]
+    new_board = []
+    for row in board:
+        new_board.append([])
+        previous = -1
+        for number in row:
+            if not number:
+                continue
+            elif number == previous:
+                new_board[-1].append(number * 2)
+                previous = -1
+            else:
+                if previous != -1:
+                    new_board[-1].append(previous)
+                previous = number
+        if previous != -1:
+            new_board[-1].append(previous)
+        new_board[-1].extend([0] * (N - len(new_board[-1])))
+    if DIR in ("right", "down"):
+        new_board = [row[::-1] for row in new_board]
+    if DIR in ("up", "down"):
+        new_board = [list(row) for row in zip(*new_board)]
+    print(f"Case #{t + 1}:")
+    for row in new_board:
+        print(' '.join(map(str, row)))
