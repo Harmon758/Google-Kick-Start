c0a6256a54212a23040d0212b31dc64b4b506510
Harmon <Harmon758@gmail.com>
2019-02-25 01:25:00 -0600

    Improve clarity of 2016 Round A Problem A solution

--- 2016/Round A/Problem A/A-solution.py
+++ 2016/Round A/Problem A/A-solution.py
@@ -1,9 +1,8 @@
 T = int(input())
 for x in range(1, T + 1):
     N = int(input())
-    y = input()
-    greatest = len(set(y.replace(" ", "")))
-    for i in range(N - 1):
+    greatest = -1
+    for i in range(N):
         name = input()
         count = len(set(name.replace(" ", "")))
         if count > greatest or (count == greatest and name < y):
