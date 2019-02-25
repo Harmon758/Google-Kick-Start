a2fb35175ba6ce68ad76e36a6a1e9020fd5da514
Harmon <Harmon758@gmail.com>
2019-02-25 02:04:14 -0600

    Improve variable names in 2017 Round B Problem A solution

--- 2017/Round B/Problem A/A-solution.py
+++ 2017/Round B/Problem A/A-solution.py
@@ -3,12 +3,12 @@ for i in range(1, 10000):
     powers_of_2.append(2 * powers_of_2[-1])
 
 T = int(input())
-for t in range(T):
+for x in range(1, T + 1):
     N = int(input())
     K = list(map(int, input().split()))
-    total = 0
+    y = 0
     for n in range(N):
         end = N - n - 1
         for i in range(end):
-            total += (K[end] - K[i]) * powers_of_2[max(end - i - 1, 0)]
-    print(f"Case #{t + 1}: {total % (10 ** 9 + 7)}")
+            y += (K[end] - K[i]) * powers_of_2[max(end - i - 1, 0)]
+    print(f"Case #{x}: {y % (10 ** 9 + 7)}")
