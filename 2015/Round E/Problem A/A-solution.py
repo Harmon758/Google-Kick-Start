bad8bd1716fd36999169e5ac9c011a2ff5e01805
Harmon <Harmon758@gmail.com>
2019-02-25 01:16:05 -0600

    Improve variable name in 2015 Round E Problem A solution

--- 2015/Round E/Problem A/A-solution.py
+++ 2015/Round E/Problem A/A-solution.py
@@ -1,7 +1,7 @@
 T = int(input())
-for t in range(T):
+for x in range(1, T + 1):
     W = input()
     answers = 1 + int(len(W) > 1 and W[0] != W[1])
     for i in range(1, len(W)):
         answers *= len(set(W[i - 1:i + 2]))
-    print(f"Case #{t + 1}: {answers % (10 ** 9 + 7)}")
+    print(f"Case #{x}: {answers % (10 ** 9 + 7)}")
