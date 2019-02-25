15ee1ae07fddbba490bad89e06f9271fd8e76b70
Harmon <Harmon758@gmail.com>
2019-02-25 01:46:52 -0600

    Improve variable names in 2017 Round A Problem A solution

--- 2017/Round A/Problem A/A-solution.py
+++ 2017/Round A/Problem A/A-solution.py
@@ -1,6 +1,6 @@
 T = int(input())
-for t in range(T):
+for x in range(1, T + 1):
     R, C = map(int, input().split())
     S1, S2 = min(R, C), max(R, C)
-    squares = (S1 * (S1 - 1) * (S1 + 1) * (2 * S2 - S1) // 12) % (10 ** 9 + 7)
-    print(f"Case #{t + 1}: {squares}")
+    y = (S1 * (S1 - 1) * (S1 + 1) * (2 * S2 - S1) // 12) % (10 ** 9 + 7)
+    print(f"Case #{x}: {y}")
