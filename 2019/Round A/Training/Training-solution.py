f789b29cacd87e29fb5bcdfc3fee517b78d680aa
Harmon <Harmon758@gmail.com>
2022-05-01 21:28:29 -0500

    Update 2019 Round A Training solution
    Use f-string

--- 2019/Round A/Training/Training-solution.py
+++ 2019/Round A/Training/Training-solution.py
@@ -9,4 +9,4 @@ for x in range(1, T + 1):
         hours += S[i] - S[P + i - 1]
         if hours < y:
             y = hours
-    print("Case #{}: {}".format(x, y), flush = True)
+    print(f"Case #{x}: {y}", flush = True)
